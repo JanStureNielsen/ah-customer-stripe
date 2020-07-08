5e66cbbdd1b751d1d4b3342346c0467cb388f879
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-07-08 06:30:18 -0600

    Remove unused imports.

--- src/main/java/com/fem/adhoc/StripeApplication.java
+++ src/main/java/com/fem/adhoc/StripeApplication.java
@@ -1,9 +1,7 @@
 package com.fem.adhoc;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StripeApplication {
