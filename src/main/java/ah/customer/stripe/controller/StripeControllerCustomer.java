1dccdd60f4598ac953879b0bb75b596dcd3d0ff8
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-07-23 16:07:16 -0600

    Use customer helper and exception handlers to simplify customer constroller.

--- src/main/java/ah/customer/stripe/controller/StripeControllerCustomer.java
+++ src/main/java/ah/customer/stripe/controller/StripeControllerCustomer.java
@@ -1,26 +1,37 @@
 package ah.customer.stripe.controller;
 
-import ah.config.StripeConfig;
-import ah.helper.StripeHelper;
-import ah.rest.AhResponse;
+import static ah.helper.AhConstant.STRIPE_REST_LARGE_LIMIT;
+import static ah.helper.StripeHelper.customerCollection;
+import static ah.helper.StripeHelper.customerCreate;
+import static ah.helper.StripeHelper.customerDelete;
+import static ah.helper.StripeHelper.customerRetrieve;
+import static ah.helper.StripeHelper.customerUpdate;
+import static ah.helper.StripeRequestHelper.ahResponseError;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.stripe.Stripe;
 import com.stripe.model.Customer;
 import com.stripe.model.CustomerCollection;
 import com.stripe.net.StripeResponse;
-import com.stripe.param.CustomerCreateParams;
-import com.stripe.param.CustomerListParams;
-import com.stripe.param.CustomerUpdateParams;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
-import static ah.helper.AhConstant.STRIPE_REST_LARGE_LIMIT;
-import static ah.helper.StripeRequestHelper.ahResponseError;
+import ah.config.StripeConfig;
+import ah.helper.StripeHelper;
+import ah.rest.AhResponse;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/customers")
 @Slf4j
 public class StripeControllerCustomer {
 
@@ -29,79 +40,48 @@ public class StripeControllerCustomer {
         Stripe.apiKey = config.stripeSecretKey();
     }
 
-    @GetMapping("/customers/all")
-    public ResponseEntity<AhResponse<Customer>> getCustomers() {
-        return getCustomers(STRIPE_REST_LARGE_LIMIT);
+    @GetMapping("/all")
+    public ResponseEntity<AhResponse<Customer>> retrieveCustomerList() {
+        return retrieveCustomerList(STRIPE_REST_LARGE_LIMIT);
     }
 
-    @GetMapping("/customers")
-    public ResponseEntity<AhResponse<Customer>> getCustomers(@RequestBody String customerListParamString) {
-        try {
-            final CustomerListParams customerListParams = StripeHelper.getGson().fromJson(customerListParamString, CustomerListParams.class);
-            final CustomerCollection customerCollection = Customer.list(customerListParams);
-            final StripeResponse lastResponse = customerCollection.getLastResponse();
-            if (lastResponse.code() == HttpStatus.OK.value()) {
-                return AhResponse.buildOk(customerCollection.getData());
-            }
-            final String errMsg = String.format("Error getting customers : Code %d \n%s", lastResponse.code(),
-                    StripeHelper.objectToJson(customerCollection));
-            log.error(errMsg);
-            return AhResponse.internalError(errMsg);
-        } catch (Exception e) {
-            log.error("Error Fetching Customer.", e);
-            return AhResponse.internalError(e);
+    @GetMapping
+    public ResponseEntity<AhResponse<Customer>> retrieveCustomerList(@RequestBody String listParam) {
+        return buildCustomerCollection(customerCollection(listParam));
+    }
+
+    @PostMapping
+    public ResponseEntity<AhResponse<Customer>> createCustomer(@RequestBody String createParam) {
+        return buildCustomer(customerCreate(createParam), "Error creating customer");
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AhResponse<Customer>> retrieveCustomer(@PathVariable("id") String customerId) {
+        return buildCustomer(customerRetrieve(customerId), "Error retrieving customer");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<AhResponse<Customer>> updateCustomer(@PathVariable("id") String customerId, @RequestBody String updateParam) {
+        return buildCustomer(customerUpdate(customerId, updateParam), "Error updating customer");
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<AhResponse<Customer>> deleteCustomer(@PathVariable("id") String customerId) {
+        return buildCustomer(customerDelete(customerId), "Error deleting customer");
+    }
+
+    private ResponseEntity<AhResponse<Customer>> buildCustomerCollection(CustomerCollection customerCollection) {
+        final StripeResponse lastResponse = customerCollection.getLastResponse();
+        if (lastResponse.code() == HttpStatus.OK.value()) {
+            return AhResponse.buildOk(customerCollection.getData());
         }
+        final String errMsg = String.format("Error getting customers : Code %d \n%s", lastResponse.code(),
+                StripeHelper.objectToJson(customerCollection));
+        log.error(errMsg);
+        return AhResponse.internalError(errMsg);
     }
 
-    @GetMapping("/customer/{id}")
-    public ResponseEntity<AhResponse<Customer>> getCustomer(@PathVariable("id") String customerCid) {
-        try {
-            final Customer customer = Customer.retrieve(customerCid);
-            return buildStripeResponseCustomer(customer, "Error fetching Customer");
-        } catch (Exception e) {
-            log.error("Error Fetching Customer.", e);
-            return AhResponse.internalError(e);
-        }
-    }
-
-    @DeleteMapping("/customer/{id}")
-    public ResponseEntity<AhResponse<Customer>> deleteCustomer(@PathVariable("id") String customerCid) {
-        try {
-            final Customer customer = Customer.retrieve(customerCid);
-            final Customer deletedCustomer = customer.delete();
-            return buildStripeResponseCustomer(deletedCustomer, "Error Removing Customer.");
-        } catch (Exception e) {
-            log.error("Error Removing Customer.", e);
-            return AhResponse.internalError(e);
-        }
-    }
-
-    @PostMapping("/customer")
-    public ResponseEntity<AhResponse<Customer>> createCustomer(@RequestBody String customerCreateParamString) {
-        try {
-            final CustomerCreateParams customerCreateParams = StripeHelper.getGson().fromJson(customerCreateParamString, CustomerCreateParams.class);
-            final Customer customerNew = Customer.create(customerCreateParams);
-            return buildStripeResponseCustomer(customerNew, "Error Creating Customer");
-        } catch (Exception e) {
-            log.error("Error Creating Customer.", e);
-            return AhResponse.internalError(e);
-        }
-    }
-
-    @PutMapping("/customer/{id}")
-    public ResponseEntity<AhResponse<Customer>> updateCustomer(@PathVariable("id") String customerCid, @RequestBody String customerUpdateParamString) {
-        try {
-            final CustomerUpdateParams customerUpdateParams = StripeHelper.getGson().fromJson(customerUpdateParamString, CustomerUpdateParams.class);
-            final Customer existingCustomer = Customer.retrieve(customerCid);
-            final Customer updatedCustomer = existingCustomer.update(customerUpdateParams);
-            return buildStripeResponseCustomer(updatedCustomer, "Error Updating Customer");
-        } catch (Exception e) {
-            log.error("Error Updating Customer.", e);
-            return AhResponse.internalError(e);
-        }
-    }
-
-    private ResponseEntity<AhResponse<Customer>> buildStripeResponseCustomer(Customer customer, String msg) {
+    private ResponseEntity<AhResponse<Customer>> buildCustomer(Customer customer, String msg) {
         final StripeResponse lastResponse = customer.getLastResponse();
         if (lastResponse.code() == HttpStatus.OK.value()) {
             try {
@@ -114,4 +94,5 @@ public class StripeControllerCustomer {
         }
         return ahResponseError(msg, lastResponse.code(), customer);
     }
+
 }
