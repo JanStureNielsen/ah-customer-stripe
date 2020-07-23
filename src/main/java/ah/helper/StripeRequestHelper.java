c665ada7038262110010fc1cfa5f461446e9fbef
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-07-23 11:39:07 -0600

    Disable construction of helper.

--- src/main/java/ah/helper/StripeRequestHelper.java
+++ src/main/java/ah/helper/StripeRequestHelper.java
@@ -7,10 +7,13 @@ import org.springframework.http.ResponseEntity;
 
 @Slf4j
 public class StripeRequestHelper {
+    private StripeRequestHelper() {
+    }
 
     public static <T extends ApiResource> ResponseEntity<AhResponse<T>> ahResponseError(String msg, int httpStatus, T entity) {
         final String errMsg = String.format("%s : Code %d \n%s", msg, httpStatus, StripeHelper.objectToJson(entity));
         log.error(errMsg);
         return AhResponse.internalError(errMsg);
     }
+
 }
