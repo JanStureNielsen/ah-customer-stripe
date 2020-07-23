8128e22274379576e0ae85404e5fc74a93fab827
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-07-23 12:05:34 -0600

    Prevent helper construction.

--- src/main/java/ah/helper/AhConstant.java
+++ src/main/java/ah/helper/AhConstant.java
@@ -1,7 +1,7 @@
 package ah.helper;
 
 public class AhConstant {
-    private static AhConstant() {
+    private AhConstant() {
     }
 
     public static final String STRIPE_REST_LARGE_LIMIT = "{ \"limit\": 9999999 }";
