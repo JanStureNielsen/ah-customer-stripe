14fb95d6c7c9bf99584b7fe75903bcc8333c6212
Jan Nielsen <jan.sture.nielsen@gmail.com>
2020-07-23 15:57:06 -0600

    Change to a builder pattern.

--- src/main/java/ah/rest/AhError.java
+++ src/main/java/ah/rest/AhError.java
@@ -1,14 +1,19 @@
 package ah.rest;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Getter;
-import org.springframework.http.HttpStatus;
-
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-@Getter
+import javax.validation.constraints.NotNull;
+
+import org.springframework.http.HttpStatus;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter @Builder
 public class AhError {
     private @NotNull final HttpStatus status;
 
@@ -17,38 +22,20 @@ public class AhError {
 
     private @NotNull final String message;
 
-    private @NotNull final String debugMessage;
+    @JsonIgnore
+    private final Throwable cause;
 
-    AhError() {
-        this(HttpStatus.OK);
-    }
-
-    AhError(HttpStatus status) {
-        this(status, null, null);
-    }
-
-    AhError(HttpStatus status, Throwable ex) {
-        this(status, "Unexpected error: ", ex);
-    }
-
-    AhError(HttpStatus status, String message) {
-        this(status, message, null, LocalDateTime.now());
-    }
-
-    AhError(HttpStatus status, String message, Throwable ex) {
-        this(status, message, ex, LocalDateTime.now());
-    }
-
-    private AhError(HttpStatus status, String message, Throwable ex, LocalDateTime timestamp) {
-        this.status = status;
-        this.message = message;
-        this.debugMessage = null != ex ? ex.getLocalizedMessage() : "No exception captured";
-        this.timestamp = timestamp;
+    public String getDebugMessage() {
+        return null != cause ? cause.getLocalizedMessage() : "No exception captured";
     }
 
     @JsonProperty("statusValue")
     public int getStatusValue() {
         return status.value();
+    }
+
+    public static class AhErrorBuilder {
+        private @NotNull LocalDateTime timestamp = LocalDateTime.now();    
     }
 
 }
